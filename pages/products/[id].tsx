08299a58a3c07a0e2f5008b82285e0bef34d1c81
Nojotel <nojotel@yandex.ru>
2024-03-02 02:50:28 +0300

    /

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { FC } from "react";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import RootLayout from "@/app/layout";
 import ProductDetails from "@/components/ProductDetails/ProductDetails";
 import ButtonBack from "@/components/ButtonBack/ButtonBack";
 
-const WrappedProductDetails = () => (
+const WrappedProductDetails: FC = () => (
   <Provider store={store}>
     <RootLayout>
       <ButtonBack />
