901301b71afd019c179d214215c6e773dfd45a6e
Nojotel <nojotel@yandex.ru>
2024-03-20 17:32:14 +0300

    fix

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styles from "./Modal.module.css";
 
 interface ModalProps {
@@ -8,35 +8,24 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ message, isOpen, onClose }) => {
-  const [show, setShow] = useState(false);
-
   useEffect(() => {
-    let timeout: ReturnType<typeof setTimeout> | null = null;
-
     if (isOpen) {
-      setShow(true);
-      timeout = setTimeout(() => {
-        setShow(false);
+      const timeout = setTimeout(() => {
         onClose();
       }, 3000);
-    } else {
-      setShow(false);
+      return () => clearTimeout(timeout);
     }
-
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-    };
   }, [isOpen, onClose]);
 
-  return show ? (
+  if (!isOpen) return null;
+
+  return (
     <div className={styles.modal}>
       <div className={styles.modalContent}>
         <span className={styles.modalMessage}>{message}</span>
       </div>
     </div>
-  ) : null;
+  );
 };
 
 export default Modal;
