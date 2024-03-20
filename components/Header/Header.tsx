c0905339f05cdd60b2b5fd5f2ab942a8632a2f4c
Nojotel <nojotel@yandex.ru>
2024-03-20 18:57:41 +0300

    fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect, useCallback } from "react";
+import React, { FC, useState, useEffect, MouseEvent, useCallback } from "react";
 import Image from "next/image";
 import styles from "./Header.module.css";
 import logo from "@/public/logo.svg";
@@ -16,6 +16,7 @@ const Header: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [logoSrc, setLogoSrc] = useState(logo);
   const [isMobile, setIsMobile] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const [showBackToTop, setShowBackToTop] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const isOpen = useSelector((state: RootState) => state.basketVisibility.isOpen);
@@ -30,7 +31,6 @@ const Header: FC = () => {
     setIsMobile(getIsMobile());
     setShowBackToTop(getIsMedium());
     setLogoSrc(logo);
-
     const handleResize = () => {
       const isMobile = getIsMobile();
       const isMedium = getIsMedium();
@@ -38,14 +38,15 @@ const Header: FC = () => {
       setShowBackToTop(isMedium);
       setLogoSrc(logo);
     };
-
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 100);
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
     };
-
     window.addEventListener("resize", handleResize);
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("scroll", handleScroll);
@@ -59,7 +60,8 @@ const Header: FC = () => {
     [dispatch]
   );
 
-  const scrollToTop = useCallback(() => {
+  const scrollToTop = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
     window.scrollTo({
       top: 0,
       behavior: "smooth",
@@ -71,27 +73,23 @@ const Header: FC = () => {
       <nav className={styles.navigation}>
         <Image className={styles.logo} src={logoSrc} alt="Logo" width={150} height={24} priority />
         <ul className={styles.links}>
-          <li className={pathname === "/" ? styles.active : ""}>
+          <li className={pathname === "/" ? styles.active : styles.linksItems}>
             <Link href="/">Товары</Link>
           </li>
-          <li className={pathname === "/orders" ? styles.active : ""}>
+          <li className={pathname === "/orders" ? styles.active : styles.linksItems}>
             <Link href="/orders">Заказы</Link>
           </li>
         </ul>
         <Basket isOpen={isOpen} onToggle={handleBasketToggle}>
-          <div className={`${styles.basketContainer} ${isOpen || isBasketPage ? styles.opened : ""}`} onClick={() => handleBasketToggle(!isOpen)}>
-            <Image className={styles.basket} src={isOpen ? basketHover : basket} alt="Корзина" width={20} height={20} />
-            {!isMobile && (
-              <>
-                <span className={styles.basketText}>Корзина</span>
-                <span className={styles.basketNumber}>({basketCount})</span>
-              </>
-            )}
+          <div className={`${styles.basketContainer} ${isOpen || isBasketPage ? styles.hovered : ""} ${isOpen ? styles.opened : ""}`} onClick={() => handleBasketToggle(!isOpen)} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <Image className={styles.basket} src={isHovered || isOpen ? basketHover : basket} alt="Корзина" width={20} height={20} />
+            {!isMobile && <span className={styles.basketText}>Корзина</span>}
+            <span style={{ color: "rgb(23, 32, 41)" }}>({basketCount})</span>
           </div>
         </Basket>
         {showBackToTop && (
           <button onClick={scrollToTop} className={`${styles.backToTop} ${isScrolled ? styles.scrolled : ""}`}>
-            <Image src={Up} alt="Back to Top" width={24} height={24} />
+            <Image src={Up} alt="Logo" width={24} height={24} />
           </button>
         )}
       </nav>
