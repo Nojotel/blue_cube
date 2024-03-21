441a9fa64376ab79617d8d650155d20562bd6143
Nojotel <nojotel@yandex.ru>
2024-03-21 17:16:18 +0300

    fix orders

---
+++ pages/orders.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header/Header";
+
+export default function Orders() {
+  return (
+    <>
+      <Header />
+      <h1 className="container">Заказы</h1>
+    </>
+  );
+}
